1070ec94d026568568f6c71cbbe867780a46c500
=aminejelass <=aminejelassi95@gmail.com>
2021-12-09 09:58:38 +0100

    fixed querys login signup

--- server/routers/login.js
+++ server/routers/login.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var db = require('../db/index')
 const crypto = require('crypto');
-
+ 
 var router = express.Router();
 
 router.route("/")
@@ -9,7 +9,7 @@ router.route("/")
 .post((req,res)=>{
     var {userName , password}=req.body
 
-    db.query("select *  from user where userName=?",[userName],(err,result)=>{
+    db.query("select *  from users where usersName=?",[userName],(err,result)=>{
         // console.log(result[0].password);
         // console.log(result[0].img);
           
