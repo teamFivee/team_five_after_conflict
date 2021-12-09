9c515fa4ba8e99df8b432d34ce027329215efd0a
FouedCharaabi <chfoued75@gmail.com>
2021-12-09 12:00:19 +0100

    seed_Data

---
+++ server/seed_data.js
@@ -0,0 +1,74 @@
+module.exports = [
+    {
+        gameName: "League Of Legends",
+        price: 40,
+        img: "https://res.cloudinary.com/team-5/image/upload/v1639044761/lol_a1rglm.jpg",
+        owner: "Garry",
+    },
+    {
+        gameName: "Fortnite",
+        price: 59,
+        img: "https://res.cloudinary.com/team-5/image/upload/v1639044823/fortnite_bwkk1s.jpg",
+        owner: "Fred",
+    },
+    {
+        gameName: "BattleField V",
+        price: 90,
+        img: "https://res.cloudinary.com/team-5/image/upload/v1639044772/battlefield_epp5dy.jpg",
+        owner: "Zack",
+    },
+    {
+        gameName: "PUBG:Player Uknowns Battleground",
+        price: 70,
+        img: "https://res.cloudinary.com/team-5/image/upload/v1639044801/pubg_otgci9.png",
+        owner: "Miqaylan",
+    },
+    {
+        gameName: "Valorant",
+        price: 50,
+        img: "https://res.cloudinary.com/team-5/image/upload/v1639044782/Valorant_hvjgjf.jpg",
+        owner: "Tenzen",
+    },
+    {
+        gameName: "Cyber Punk 2077",
+        price: 200,
+        img: "https://res.cloudinary.com/team-5/image/upload/v1639044899/cyberpunk-2077-button-fin-1594877291453_ckkjuu.jpg",
+        owner: "Farouk",
+    },
+    {
+        gameName: "Red Dead Redemption II",
+        price: 130,
+        img: "https://res.cloudinary.com/team-5/image/upload/v1639045293/Hpl5MtwQgOVF9vJqlfui6SDB5Jl4oBSq_xzitq8.jpg",
+        owner: "Jhon",
+    },
+    {
+        gameName: "World of Warcraft",
+        price: 35,
+        img: "https://res.cloudinary.com/team-5/image/upload/v1639045613/world-of-warcraft-classic-button-01-1566253968524_azs1if.jpg",
+        owner: "Greg",
+    },
+    {
+        gameName: "Need For Speed : Heat",
+        price: 50,
+        img: "https://res.cloudinary.com/team-5/image/upload/v1639046550/nfs-heat---button-fin-1565806445973_dwjyix.jpg",
+        owner: "Casper",
+    },
+    {
+        gameName: "Grand Theft Auto V online",
+        price: 79,
+        img: "https://res.cloudinary.com/team-5/image/upload/v1639046841/mobile-cover_s6awyu.jpg",
+        owner: "Spencer",
+    },
+    {
+        gameName: "Guardians Of the Galaxy",
+        price: 200,
+        img: "https://res.cloudinary.com/team-5/image/upload/v1639045072/marvels-guardians-of-the-galaxy-pc-game-steam-cover_fmxo1r.jpg",
+        owner: "Tarik",
+    },
+    {
+        gameName: "Dark Souls III",
+        price: 60,
+        img: "https://res.cloudinary.com/team-5/image/upload/v1639045176/v8JlD8KcQUtTqaLBmpFnj1ESRR5zMkLk_jxhalt.jpg",
+        owner: "Aymen",
+    },
+];
