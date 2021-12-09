1247ecc2dbf101f697cf92412b78974df0344f2f
jouinibilel <bileljouinipro@hotmail.com>
2021-12-09 11:31:36 +0100

    signup half done

--- g2a/src/components/pages/SignUp.jsx
+++ g2a/src/components/pages/SignUp.jsx
@@ -1,6 +1,42 @@
+import axios from "axios";
 import React from "react";
 import "../../App.css";
 
-export default function SignUp() {
-    return <h1 className="sign-up">hellooo</h1>;
+export default class SignUp extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            userName:'',
+            Email:'',
+            password:''
+        }
+        this.username=this.username.bind(this)
+        this.email=this.email.bind(this)
+        this.password=this.password.bind(this)
+        this.singnup=this.singnup.bind(this)
+    }
+    username(e){this.setState({userName:e.target.value})}
+    email(e){this.setState({Email:e.target.value})}
+    password(e){this.setState({password:e.target.value})}
+    singnup(){
+        const userName=this.state.userName
+        const Email=this.state.Email
+        const password=this.state.password
+        axios.post('http://localhost:5000/api/signup',{userName,Email,password}).then(response=>{
+            console.log(response)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+    render(){
+        return(
+        <div>
+            <label htmlFor="">username<br /><input type="name" onChange={this.username} /></label><br />
+            <label htmlFor="">email<br /><input type="email" onChange={this.email}/></label><br />
+            <label htmlFor="">password<br /><input type="password" onChange={this.password}/></label><br />
+            <button onClick={this.singnup}>signup</button>
+            
+        </div>
+        )
+    }
 }
--- g2a/src/index.js
+++ g2a/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import Main from './main.jsx';
+import App from './App';
+
 
 ReactDOM.render(
   <React.StrictMode>
-   <Main />
+  <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
