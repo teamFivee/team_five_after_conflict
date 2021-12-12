2549520bc24eddeaf103f85224b4eda0628fe17d
HA-Amir <you@example.com>
2021-12-12 16:17:27 +0100

    merge 2

--- g2a/src/components/pages/SignUp.jsx
+++ g2a/src/components/pages/SignUp.jsx
@@ -9,12 +9,13 @@ export default class SignUp extends React.Component{
             userName:'',
             Email:'',
             password:'',
+            imageSelected:'',
         }
         this.username=this.username.bind(this)
         this.email=this.email.bind(this)
         this.password=this.password.bind(this)
         this.singnup=this.singnup.bind(this)
-        // this.uploadImage=this.uploadImage.bind(this)
+      
         
     }
     username(e){
@@ -23,34 +24,22 @@ export default class SignUp extends React.Component{
     }
     email(e){this.setState({Email:e.target.value})}
     password(e){this.setState({password:e.target.value})}
-//     uploadImage(files){
-//         const body = new FormData()
-// files = files[0]
-// body.append("file", files)
-// body.append("upload_preset", 'lxhgxbut')
-
-// this.state.img =  fetch('https://api.cloudinary.com/v1_1/g2a2/upload', {
-//   method: "POST",
-//   body: body
-// })
-
-// console.log(img)
-        // console.log(files[0])
-        // const formData= new FormData()
-        // formData.append=('file',files[0])
-        // formData.append=('upload_preset','o6uj3jxx')
-        // axios.post('https://api.cloudinary.com/v1_1/tabbabighassen/image/upload',formData).then(response=>{
-        //  console.log(response , "resposee")
-        // }).catch(err=>{
-        //     console.log(err)
-        // })
-    // }
-     
+    
+    
+   
+        // const [imageSelected,setImageSelected]=useState("")
+       uploadImage=() =>{
+       const formData= new FormData()
+       formData.append("file",imageSelected);
+       formData.append('upload_preset', 'lsom30en');
+       axios.post('https://api.cloudinary.com/v1_1/ben-arous/image/upload',formData).then((response)=>console.log(response))
+   
+     }
 
     singnup(){
         const userName=this.state.userName
         const Email=this.state.Email
-        // const balance = this.state.balance;
+     
         const password=this.state.password
         
         axios.post('http://localhost:5000/api/signup',{userName,Email,password}).then(response=>{
